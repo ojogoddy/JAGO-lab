61fd34e899afa7b33d52ddc940d65aceb2f102ca
ojogoddy <goddy5867@gmail.com>
2023-07-26 03:00:32 +0100

    more pages

---
+++ signup.js
@@ -0,0 +1,15 @@
+document.getElementById("signup-btn").addEventListener("click", function(e){
+    e.preventDefault();
+    let uName = document.getElementById("username").value;
+    let uEmail = document.getElementById("email").value;
+    let uPass = document.getElementById("password").value;
+
+    if(uName.length === 0|| uEmail.length === 0|| uPass.length===0)
+    {
+        alert("Please enter your full details")
+    }else{
+        localStorage.setItem("username", uName)
+        localStorage.setItem("email", uEmail)
+        localStorage.setItem("password", uPass)
+    }
+})
